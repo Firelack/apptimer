e7c6fc0cb127c9583a185722f216b75e677a7ece
Firelack <firelack2000@gmail.com>
2025-01-10 08:42:50 +0100

    import correct label

--- routineapp.py
+++ routineapp.py
@@ -2,6 +2,9 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.clock import Clock
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.scrollview import ScrollView
 
 
 class RoutineApp(App):
