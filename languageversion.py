75a186fd42b35c07e8e963b2e6dad1980e7ebcfe
Firelack <firelack2000@gmail.com>
2025-05-02 17:37:58 +0200

    New json file for different language

--- languageversion.py
+++ languageversion.py
@@ -165,46 +165,9 @@ class StyledButton(FocusBehavior, HoverBehavior, Button):
 
 class RoutineApp(App):
     FILE_PATH = "routinesV3.json"  # Définition du chemin du fichier JSON
-    dictlanguage = {
-    "English": {
-        "welcome_page": "Welcome to this personalized routine application",
-        "home_page": "Add a routine",
-        "confirmation": ["Confirmation", "Do you really want to delete the routine ", "Yes", "No"],
-        "add_routine": ["Routine name :", "Finish", "Cancel"],
-        "routine_page": ["Routine :", "rep", "s", "units", "Start", "Add", "Back"],
-        "start_routine": ["Preparing...", "Done", "Pause", "Skip rest", "Back"],
-        "toggle_pause": ["Resume", "Pause"],
-        "update_routine": ["Routine completed !", "Rest :", "s", "Repetition ", " units"],
-        "change_routine": [
-            "Add an exercise :", "Exercise name :", "Duration (seconds) :", "Repetitions :", "Rest (seconds) :",
-            "Units : (if no duration)", "Add the exercise", "Back", "Duration/units :", "Please enter a valid name for the exercise",
-            "Duration must be greater than 0 or must be specified", "Repetitions must be greater than 0 or must be specified", 
-            "Rest time cannot be negative or must be specified", "Please enter a valid number for units", 
-            "Please enter a valid number for the duration", "Please enter a valid number for the repetitions", 
-            "Please enter a valid number for the rest","No duration/units specified"
-        ]
-    },
-    "French": {
-        "welcome_page": "Bienvenue dans cette application de routine personnalisée",
-        "home_page": "Ajouter une routine",
-        "confirmation": ["Confirmation", "Voulez-vous vraiment supprimer la routine ", "Oui", "Non"],
-        "add_routine": ["Nom de la routine :", "Terminer", "Annuler"],
-        "routine_page": ["Routine :", "rep", "s", "unités", "Lancer", "Ajouter", "Retour"],
-        "start_routine": ["Préparation...", "Fait", "Pause", "Passer le repos", "Retour"],
-        "toggle_pause": ["Relancer", "Pause"],
-        "update_routine": ["Routine terminée !", "Repos :", "s", "Répétition ", " unités"],
-        "change_routine": [
-            "Ajouter un exercice :", "Nom de l'exercice :", "Durée (secondes) :", "Répétitions :", "Repos (secondes) :",
-            "Unités : (si pas de durée)", "Ajouter l'exercice", "Retour", "Durée/unités :", 
-            "Veuillez entrer un nom valide pour l'exercice", "La durée doit être supérieure à 0 ou doit être spécifiée", 
-            "Les répétitions doivent être supérieures à 0 ou doivent être spécifiées", 
-            "Le temps de repos ne peut pas être négatif ou doit être spécifié", 
-            "Veuillez entrer un nombre valide pour les unités", "Veuillez entrer un nombre valide pour la durée", 
-            "Veuillez entrer un nombre valide pour les répétitions", "Veuillez entrer un nombre valide pour le temps de repos",
-            "Aucune durée/unités spécifiée"
-        ]
-    }
-}
+
+    with open("language.json", 'r', encoding='utf-8') as f:
+        dictlanguage = json.load(f)
 
 
 
